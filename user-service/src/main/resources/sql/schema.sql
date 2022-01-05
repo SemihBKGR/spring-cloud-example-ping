ac6bac8da554f45c2ae43e51406c8e93aa66f622
SemihBKGR <buyukgungorsemih@gmail.com>
2022-01-05 23:11:06 +0300

    adds uaa service and creates req docker compose file contains only additional services

---
+++ user-service/src/main/resources/sql/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `users`
+(
+    `id`       INT PRIMARY KEY AUTO_INCREMENT,
+    `username` VARCHAR(64) UNIQUE NOT NULL,
+    `password` VARCHAR(256)       NOT NULL,
+    `email`    VARCHAR(64) UNIQUE NOT NULL
+)
