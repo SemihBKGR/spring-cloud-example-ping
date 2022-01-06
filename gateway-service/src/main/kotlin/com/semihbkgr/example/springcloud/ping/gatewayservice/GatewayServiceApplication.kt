b9ce7a6ab5fc9ce06ae3ccf4b7ad51106eaaee5c
SemihBKGR <buyukgungorsemih@gmail.com>
2022-01-06 03:46:55 +0300

    init gateway service add its configurations

---
+++ gateway-service/src/main/kotlin/com/semihbkgr/example/springcloud/ping/gatewayservice/GatewayServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.semihbkgr.example.springcloud.ping.gatewayservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<GatewayServiceApplication>(*args)
+}
